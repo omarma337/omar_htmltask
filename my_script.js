3d9fe6512633be36f53a73cda65756ef4ea71171
omarma337 <omarabojolban@gmail.com>
2024-07-17 10:10:56 +0300

    task 3 htmlpage + css

---
+++ my_script.js
@@ -0,0 +1,20 @@
+var mine="omar" ;
+let user_num=1;
+const user_id=2020 ;
+
+let obj={
+name:"oamr",
+age:25
+};
+//this a comment
+let arr=[1,"im"];
+
+console.log(arr[1]);
+alert("welcome ${name}");
+let name_input=prompt("what is your name");
+
+
+//////////////////////////donot you ever use var for any define to variable
+// defination function , expression and arrow
+// hoisting
+// doument.write inner.html
